fb306ade269c97aef6f9ac02d0531dd6d87258f8
sonic <sonic@Isaiahs-MacBook-Pro.local>
2020-07-30 18:22:52 -0400

    Added button component

--- src/pages/Homepage/Homepage-component.jsx
+++ src/pages/Homepage/Homepage-component.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import PageSection from '../../components/PageSection/PageSection-component';
+import Button from '../../components/Button/Button-component';
 
 const Homepage = ({ ...props }) => {
   return (
     <div>
       <h1>Homepage</h1>
       <PageSection />
+      <Button buttonClass="primary" value="Learn More"/>
+      <Button buttonClass="outline" value="Learn More"/>
     </div>
   );
 };
