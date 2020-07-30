d0671d6c52e072170fcaa369b3ac6b0f1c18985a
sonic <sonic@Isaiahs-MacBook-Pro.local>
2020-07-30 18:07:18 -0400

    Removed ProfileImage from button branch

--- src/components/ProfileImage/ProfileImage-component.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react"
-import "./ProfileImage.scss"
-const ProfileImage = ({image}) => {
-    return(<img className="profile-image" src={image} alt="profile"/>)
-}
-
-export default ProfileImage
