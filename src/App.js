444b33316535ed2ba6fbdb97de69fd1199bed0ff
Abaraham Ndinga <40493309+AbrahamDN@users.noreply.github.com>
2020-07-31 21:35:17 +0100

    Removed ProfileImage from App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React from 'react';
 import './App.scss';
 import Homepage from './pages/Homepage/Homepage-component';
-import ProfileImage from './components/ProfileImage/Profile-Image-component';
-
 class App extends React.Component {
   render() {
     return (
       <div className='App'>
         <Homepage />
-        <ProfileImage image={"https://www.freeiconspng.com/thumbs/profile-icon-png/profile-icon-9.png"}/>
       </div>
     );
   }
 }
-
 
 export default App;
