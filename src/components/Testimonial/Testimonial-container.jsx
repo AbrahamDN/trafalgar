921861d9c94b753ee506e880737f69020030c35c
sonic <sonic@Isaiahs-MacBook-Pro.local>
2020-07-31 18:36:19 -0400

    [TRAF-9] Created Testimonial Container

---
+++ src/components/Testimonial/Testimonial-container.jsx
@@ -0,0 +1,33 @@
+import React from "react"
+import "./Testimonial-container-styles.scss"
+import ProfileImage from "../ProfileImage/Profile-Image-component"
+const Testimonial = ({title,name,info,review}) => {
+    return(
+        <div className="testimonial">
+            <div className="container">
+                <h1 className="testimonial-title">{title}</h1>
+
+                <div className="testimonial-main">
+
+                    <div className="testimonial-profile">
+                        <ProfileImage/>
+                        <div className="testimonial-profile-info">
+                            <h4 className="testimonial-name">{name}</h4>
+                            <p className="testimonial-info">{info}</p>
+                        </div>
+                    </div>
+
+                    <div className="testimonial-review">
+                        <p>{review}</p>
+                    </div>
+
+                </div>
+
+
+            </div>
+
+        </div>
+    )
+}
+
+export default Testimonial
