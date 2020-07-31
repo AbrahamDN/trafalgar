778bf24acaa3297050d2abd31d85f359aeca77b2
sonic <sonic@Isaiahs-MacBook-Pro.local>
2020-07-30 21:41:13 -0400

    Created Button component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.scss';
 import Homepage from './pages/Homepage/Homepage-component';
 import ProfileImage from './components/ProfileImage/Profile-Image-component';
+import Button from './components/Button/Button-component';
 
 class App extends React.Component {
   render() {
@@ -9,6 +10,8 @@ class App extends React.Component {
       <div className='App'>
         <Homepage />
         <ProfileImage image={"https://www.freeiconspng.com/thumbs/profile-icon-png/profile-icon-9.png"}/>
+        <Button value="Learn More" buttonClass="primary"/>
+        <Button value="Learn More" buttonClass="outline"/>
       </div>
     );
   }
--- src/pages/Homepage/Homepage-component.jsx
+++ src/pages/Homepage/Homepage-component.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import PageSection from '../../components/PageSection/PageSection-component';
 import SplitContent from '../../components/SplitContent/SplitContent-container';
-
 const Homepage = ({ ...props }) => {
   return (
     <div>
