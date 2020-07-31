389d65e5dd504b0fa064a95b85a9d078605ddbbf
sonic <sonic@Isaiahs-MacBook-Pro.local>
2020-07-30 22:06:54 -0400

    Created Button Component and removed the HomePage Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.scss';
-import Homepage from './pages/Homepage/Homepage-component';
 import ProfileImage from './components/ProfileImage/Profile-Image-component';
 import Button from './components/Button/Button-component';
 
@@ -8,7 +7,6 @@ class App extends React.Component {
   render() {
     return (
       <div className='App'>
-        <Homepage />
         <ProfileImage image={"https://www.freeiconspng.com/thumbs/profile-icon-png/profile-icon-9.png"}/>
         <Button value="Learn More" buttonClass="primary"/>
         <Button value="Learn More" buttonClass="outline"/>
