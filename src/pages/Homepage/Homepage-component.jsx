32510f01cb4ab32e405d66415cfb3d79ff22058d
Abaraham Ndinga <40493309+AbrahamDN@users.noreply.github.com>
2020-08-01 05:22:51 +0100

    [TRAF-19] Update d component w/ Header, Button component & some copy text

--- src/pages/Homepage/Homepage-component.jsx
+++ src/pages/Homepage/Homepage-component.jsx
@@ -1,16 +1,32 @@
 import React from 'react';
+
+import Header from '../../components/Header/Header-component';
 import PageSection from '../../components/PageSection/PageSection-component';
 import SplitContent from '../../components/SplitContent/SplitContent-container';
+import Button from '../../components/Button/Button-component';
 
-const Homepage = ({ ...props }) => {
+import './Homepage-styles.scss';
+
+const Homepage = () => {
   return (
-    <div>
-      <h1>Homepage</h1>
+    <div className='homepage'>
+      <Header />
       <PageSection>
-        <h2>Section Component</h2>
         <SplitContent>
-          <div>Split</div>
-          <div>Content</div>
+          <div>
+            <h1>Virtual healthcare for you</h1>
+            <p>
+              Trafalgar provides progressive, and affordable healthcare,
+              accessible on mobile and online for everyone
+            </p>
+            <Button buttonClass='outline' value='Consult today' />
+          </div>
+          <div>
+            <img
+              src='/assets/png/trafalgar-header illustration 1.png'
+              alt='hero'
+            />
+          </div>
         </SplitContent>
       </PageSection>
     </div>
