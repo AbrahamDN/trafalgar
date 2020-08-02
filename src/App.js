127b6c6b6ffd4a4e52238c751bbc50e3bd9665e8
Abaraham Ndinga <40493309+AbrahamDN@users.noreply.github.com>
2020-08-02 14:39:04 +0100

    [TRAF-20] Added Navbar & Navbar links object to state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
 import React from 'react';
 import './App.scss';
 import Homepage from './pages/Homepage/Homepage-component';
+import Navbar from './components/NavBar/Navbar-component';
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      navLinks: [
+        {
+          name: 'Home',
+          url: '/',
+          active: true,
+        },
+        {
+          name: 'Find a doctor',
+          url: '/',
+        },
+        {
+          name: 'Apps',
+          url: '/',
+        },
+        {
+          name: 'Testimonials',
+          url: '/',
+        },
+        {
+          name: 'About us',
+          url: '/',
+        },
+      ],
+    };
+  }
+
   render() {
+    const { navLinks } = this.state;
     return (
       <div className='App'>
+        <Navbar className='navbar-wrap' links={navLinks} />
         <Homepage />
       </div>
     );
