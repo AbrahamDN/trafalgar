0aa77e1b72fb4a76b4d62ea56783b63b2ac3656e
Abaraham Ndinga <40493309+AbrahamDN@users.noreply.github.com>
2020-08-02 13:40:17 +0100

    [TRAF-24] Updated Navbar to use NavLinks

--- src/components/NavBar/Navbar-component.jsx
+++ src/components/NavBar/Navbar-component.jsx
@@ -1,20 +1,19 @@
-import React from "react"
-import "./Navbar-styles.scss"
-const Navbar= () => {
-    return(
-        <div className="header">
-            <img src="/assets/png/logo.png"/>
-            <nav>
-                <ul className="nav-list">
-                    <li className="nav-item"><a className="nav-link" href="#">Home</a></li>
-                    <li className="nav-item"><a className="nav-link" href="#">Find a doctor</a></li>
-                    <li className="nav-item"><a className="nav-link" href="#">Apps</a></li>
-                    <li className="nav-item"><a className="nav-link" href="#">Testimonials</a></li>
-                    <li className="nav-item"><a className="nav-link" href="#">About us</a></li>
-                </ul>
-            </nav>
-        </div>
-    )
-}
+import React from 'react';
 
-export default Navbar
+import { ReactComponent as Logo } from '/assets/logo.svg';
+import './Navbar-styles.scss';
+
+import NavLinks from '../NavLinks/NavLinks-component';
+
+const Navbar = ({ links, ...otherProps }) => {
+  return (
+    <div className='header' {...otherProps}>
+      <Logo />
+      <nav>
+        <NavLinks links={links} className='nav-list' />
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
