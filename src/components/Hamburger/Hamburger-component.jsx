b35bd1b280a12287d9024b15b5e087e2e84ac81f
sonic <sonic@Isaiahs-MacBook-Pro.local>
2020-08-05 17:46:47 -0400

    [TRAF-12] Updated Hamburger Component

--- src/components/Hamburger/Hamburger-component.jsx
+++ src/components/Hamburger/Hamburger-component.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react"
 import "./Hamburger-styles.scss"
 const Hamburger = ({shown,onClick }) => {
         return(
-            <div>
+            <div className="close">
                 {shown ?
                  <a className="hamburger" href="#" onClick={onClick}>&#9776;</a>:
                  <a className="times" href="#" onClick={onClick}>&times;</a>
