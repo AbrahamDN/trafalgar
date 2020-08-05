e342a4ca0b826d2f92b78da27031e2cafd435868
Abaraham Ndinga <40493309+AbrahamDN@users.noreply.github.com>
2020-08-05 19:09:39 +0100

    [TRAF-28] Updated html structure & styling

--- src/components/Testimonial/Testimonial-container.jsx
+++ src/components/Testimonial/Testimonial-container.jsx
@@ -1,34 +1,37 @@
-import React from "react"
-import "./Testimonial-container-styles.scss"
-import ProfileImage from "../ProfileImage/Profile-Image-component"
-const Testimonial = ({title,name,info,review}) => {
-    return(
-        <div className="testimonial">
-             <h1 className="testimonial-title">{title}</h1>
-            <div className="container">
-               
-
-                <div className="testimonial-main">
-
-                    <div className="testimonial-profile">
-                        <ProfileImage/>
-                        <div className="testimonial-profile-info">
-                            <h4 className="testimonial-name">{name}</h4>
-                            <p className="testimonial-info">{info}</p>
-                        </div>
-                    </div>
-
-                    <div className="testimonial-review">
-                        <p>{review}</p>
-                    </div>
-
-                </div>
-
-
+import React from 'react';
+import './Testimonial-container-styles.scss';
+import ProfileImage from '../ProfileImage/Profile-Image-component';
+const Testimonial = ({
+  title,
+  name,
+  info,
+  review,
+  image,
+  alt,
+  ...otherProps
+}) => {
+  return (
+    <div className='testimonial' {...otherProps}>
+      <div className='testimonial-content'>
+        <h1>{title}</h1>
+        <hr />
+        <div className='testimonial-main'>
+          {name && image ? (
+            <div className='testimonial-info'>
+              <ProfileImage image={image} alt={alt} />
+              <span>
+                <h3>{name}</h3>
+                <p>{info}</p>
+              </span>
             </div>
-
+          ) : null}
+          <div className='testimonial-review'>
+            <p>{`"${review || 'Review'}"`}</p>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Testimonial
+export default Testimonial;
