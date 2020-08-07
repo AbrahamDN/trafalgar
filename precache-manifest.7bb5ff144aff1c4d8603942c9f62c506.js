e3b05fc902bb8f1bf6f2f71b84b2615a700f81fc
Abaraham Ndinga <40493309+AbrahamDN@users.noreply.github.com>
2020-08-08 00:17:43 +0100

    Updates

---
+++ precache-manifest.7bb5ff144aff1c4d8603942c9f62c506.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f328e2fd5362b95f9ae9a3a211d3b9b0",
+    "url": "/trafalgar/index.html"
+  },
+  {
+    "revision": "23d995d149f373b3b2b6",
+    "url": "/trafalgar/static/css/main.95421303.chunk.css"
+  },
+  {
+    "revision": "0e14ecf3c7f4c706b9ff",
+    "url": "/trafalgar/static/js/2.aee02f4d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/trafalgar/static/js/2.aee02f4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23d995d149f373b3b2b6",
+    "url": "/trafalgar/static/js/main.ecb9e085.chunk.js"
+  },
+  {
+    "revision": "298d11db11de8b5fa5b7",
+    "url": "/trafalgar/static/js/runtime-main.1252dfe1.js"
+  },
+  {
+    "revision": "1e36ee6d50c037b1bb4bdd3effa7a28e",
+    "url": "/trafalgar/static/media/Muli-Bold.1e36ee6d.ttf"
+  },
+  {
+    "revision": "6b667c3dbc3d4df6dd096ad18296704d",
+    "url": "/trafalgar/static/media/Muli-Light.6b667c3d.ttf"
+  },
+  {
+    "revision": "df7330254513d2fa2f4c1e9ee98cc6c6",
+    "url": "/trafalgar/static/media/Muli.df733025.ttf"
+  },
+  {
+    "revision": "71c0d5e84f291a1a8e34d900f6ef4aef",
+    "url": "/trafalgar/static/media/arrow.71c0d5e8.svg"
+  }
+]);
--- src/components/PageSection/PageSection-component.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import './PageSection-styles.scss';
-
-const PageSection = ({ children, ...otherProps }) => (
-  <section {...otherProps}>{children}</section>
-);
-
-export default PageSection;
